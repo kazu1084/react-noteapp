d809471d2ff884c2032b1c5d0a483507c4b7115a
EC2 Default User <ec2-user@ip-172-31-0-87.ap-northeast-1.compute.internal>
2025-01-31 06:36:43 +0000

    mapmethod-deletemethod

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import './App.css'
 import Main from './components/Main.jsx';
 import Sidebar from './components/Sidebar.jsx';
+import React, { useState } from 'react';
+import uuid from "react-uuid";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+  const onAddNote = () => {
+     console.log("新しいノートが追加されました。") 
+     const newNote = {
+       id: uuid(),
+       title: "新しいノート",
+       content:"内容をここに書きます。",
+       modDate: Date.now(),
+     };
+     setNotes([...notes, newNote]);
+     console.log(notes);
+    };
+    
+  const onDeleteNote = (id) => {
+    const filterNotes = notes.filter((note) => note.id !== id);
+    setNotes(filterNotes);
+  };
+  
   return (
     <div className = "App">
-      <Sidebar />
+      <Sidebar onAddNote = {onAddNote} notes = {notes} onDeleteNote = {onDeleteNote} />
       <Main />
     </div>
   )
